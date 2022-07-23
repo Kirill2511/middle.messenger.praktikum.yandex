213604e0c675192ea9d28da2bb720463c150194b
Kirill <kirill.kirill2@yandex.ru>
2022-07-24 00:51:44 +0500

    sprint_1

---
+++ src/pages/login/login.js
@@ -0,0 +1,11 @@
+import './login.scss';
+
+window.onload = () => {
+  const registrationForm = document.querySelector('.form');
+  registrationForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const fd = new FormData(registrationForm);
+    console.log(Object.fromEntries(fd.entries()));
+    window.location.href = '/pages/chat/chat.pug';
+  });
+};
